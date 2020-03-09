645f83c51da8df4eee130465e22151bf85e38f4c
Komal Dindiyal <komald.4@gmail.com>
2020-03-09 20:04:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // Code your solutions in this file
-const writeCards()
+function writeCards(namesArray, event) {
+  let thankYouCards = []
+  for (let i = 0; i < namesArrayarray.length; i++) {
+    thankYouCards.push('Thank you, $[cards[i]], for the wonderful birthday gift!') ;
+  }
+  return thankYouCards;
+}
+
+
+
+
+ 
+function countDown( startingNumber) {
+    while ( starting Number > 0 ) {
+        console.log( startingNumber);
+        startingNumber -=1;
+    }
+    console.log(startingNumber);
+}
